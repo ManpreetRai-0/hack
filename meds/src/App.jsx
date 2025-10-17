03ff3af59fcf44f845ef521a48c0e990f4cda980
Manpreet Rai <manpreetrai9000@gmail.com>
2025-10-17 14:01:27 -0700

    folder changes

--- meds/src/App.jsx
+++ meds/src/App.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './sign-in/SignIn'
-import SignIn from './sign-in/SignIn'
+import './Pages/sign-in/SignIn'
+import SignIn from './Pages/sign-in/SignIn'
 import { Routes, Route } from 'react-router';
 
 function App() {
