cce6323c5a927d656f91dc9ad898b3c81405b193
Manpreet Rai <manpreetrai9000@gmail.com>
2025-10-17 15:30:38 -0700

    dashboard

--- meds/src/App.jsx
+++ meds/src/App.jsx
@@ -5,6 +5,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './Pages/sign-in/SignIn'
 import SignIn from './Pages/sign-in/SignIn'
+import './Pages/dashboard/Dashboard'
+import Dashboard from './Pages/dashboard/Dashboard'
 import { Routes, Route } from 'react-router';
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         
       </Routes>
     </>
